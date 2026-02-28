ae45e71d31672e6132b8587e72423fc850d02e0e
viasuvio-ops <viasuvio@gmail.com>
2026-02-28 19:15:53 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 }
+  server: {
+    host: true,
+    port: 5173
+  },
+  preview: {
+    host: true,
+    port: process.env.PORT || 4173,
+    allowedHosts: true
+  }
 });
